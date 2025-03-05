d3a916bcf50b33ddabba2f3430486e6c6e985c5d
rc4317809 <rc4317809@gmail.com>
2025-03-05 14:13:59 +0800

    new update

--- app/Filament/Resources/StudentResource.php
+++ app/Filament/Resources/StudentResource.php
@@ -59,9 +59,9 @@ class StudentResource extends Resource
                     ->sortable()
                     ->badge()
                     ->label('Department'),
-                TextColumn::make('student_id')->sortable()->searchable()->searchable('student_id'),
-                TextColumn::make('first_name')->sortable()->searchable()->searchable('first_name'),
-                TextColumn::make('last_name')->sortable()->searchable()->searchable('last_name'),
+                TextColumn::make('student_id')->sortable()->searchable()->placeholder('Search by Student ID'),
+                TextColumn::make('first_name')->sortable()->searchable()->placeholder('Search by First Name'),
+                TextColumn::make('last_name')->sortable()->searchable()->placeholder('Search by Last Name'),
                 TextColumn::make('elections')
                     ->label('Assigned Elections')
                     ->formatStateUsing(fn($record) => $record->elections->pluck('name')->join(', ')),
