2d074b4f726c2f0a200395470eb35ba54045a2e4
rc4317809 <rc4317809@gmail.com>
2025-03-15 16:55:46 +0800

    wip

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -37,7 +37,7 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->font('Poppins')
             ->favicon(asset('images/favico.ico'))
-            ->brandName('APEC')
+            ->brandName('apec')
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
